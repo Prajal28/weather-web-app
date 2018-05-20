ce117f5c44fcd94824347a1bf2ec8ff28cda97af
Prajal Mishra <prajalmishra24@gmail.com>
2018-05-20 15:13:32 -0500

    localtogit

---
+++ weather.js
@@ -0,0 +1,19 @@
+
+
+var api_key = "98f453b22fb8e2d7cc313779de835b38";
+
+function sendRequest () {
+    var xhr = new XMLHttpRequest();
+   // var method = "artist.getinfo";
+    var city = encodeURI(document.getElementById("form-input").value);
+    xhr.open("GET", "proxy.php?q="+city+"&appid="+api_key+"&format=json", true);
+    xhr.setRequestHeader("Accept","application/json");
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            var json = JSON.parse(this.responseText);
+            var str = JSON.stringify(json,undefined,2);
+            document.getElementById("output").innerHTML = "<pre>" + str + "</pre>";
+        }
+    };
+    xhr.send(null);
+}
